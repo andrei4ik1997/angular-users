7945d2a1f5d5de98a421748ec9baccc7ef4a0781
Andrei Shynkarou <andrey03071997@yandex.ru>
2024-11-20 23:27:47 +0300

    Auto-generated commit

---
+++ chunk-4S2B5ULX.js
@@ -0,0 +1,1 @@
+import{c as o}from"./chunk-AXD3V6Z4.js";import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:async()=>import("./chunk-C5HP7HCC.js"),title:o.notFound}],e=t;export{e as default};
---
+++ chunk-B2JR3GLO.js
@@ -0,0 +1,1 @@
+import{c as t}from"./chunk-AXD3V6Z4.js";import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:async()=>import("./chunk-DW2EUUC5.js"),title:t.users}],e=o;export{e as default};
